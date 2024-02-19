2f83b99244fd8de6b283e1c50c743a20299ae901
유희주 <148019108+yooheeju@users.noreply.github.com>
2024-02-19 16:01:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		
+		long A,B,C; // 문제 조건에 10^12 까지라고 주어짐.
+		A = scanner.nextLong();
+		B = scanner.nextLong();
+		C = scanner.nextLong();
+		
+		System.out.println(A+B+C); 
+		
+	}
+}
