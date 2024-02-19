c7aef452578c6a94c90e90df811412bd64052178
유희주 <148019108+yooheeju@users.noreply.github.com>
2024-02-19 15:22:07 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		
+		String id = scanner.nextLine();
+		
+        System.out.println(id+"??!");
+		
+	}
+}
