549e32cca49f8987a08d701f28f9963b7017a1fa
유희주 <148019108+yooheeju@users.noreply.github.com>
2024-02-19 14:50:12 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A+B);
+	}
+
+}
