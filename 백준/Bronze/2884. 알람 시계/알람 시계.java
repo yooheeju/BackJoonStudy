ef28ede74c0b0ef151f9ec89fc9637ddd1828370
유희주 <148019108+yooheeju@users.noreply.github.com>
2024-02-22 16:45:12 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int h = scanner.nextInt();
+		int m = scanner.nextInt();
+		
+		if(m>=45) {
+			System.out.println(h+" "+(m-45));
+		}else{
+			if(h<1) {
+				h=24;
+			}
+			System.out.println(h-1+" "+((60+m)-45));
+		}
+	}
+}
